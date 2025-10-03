b43f3a5dcd0a91f99d33315db038ac12197536d7
omaroymk <72468295+oymk0@users.noreply.github.com>
2025-10-03 22:40:32 +0300

    Added some attributes and constructor in account class

---
+++ Assignments/Assignment_01/src/Question_9/Account.java
@@ -0,0 +1,33 @@
+package Question_9;
+
+import java.util.Date;
+
+class Account
+{
+    // attributes
+    private int id;
+    private double balance;
+    private static double annualInterestRate;
+    private Date dateCreated;
+
+    // constructors
+    public Account() {}
+    public Account(int id, double balance)
+    {
+        this.id = id;
+        this.balance = balance;
+    }
+
+    // getters
+    public int getId() { return id; }
+    public double getBalance() { return balance; }
+    public double getAnnualInterestRate() { return annualInterestRate; }
+    public Date  getDateCreated() { return dateCreated; }
+
+    // setters
+    public void setId(int id) { this.id = id; }
+    public void setBalance(double balance) { this.balance = balance; }
+    public void setAnnualInterestRate(double annualInterestRate) { this.annualInterestRate = annualInterestRate; }
+
+    // methods
+}
