b255c9fcd439580ff932eed40f4734ebec559563
omaroymk <72468295+oymk0@users.noreply.github.com>
2025-10-17 22:09:46 +0300

    Added Assignment_02

---
+++ Assignments/Assignment_02/src/Question_5/MyInteger.java
@@ -0,0 +1,15 @@
+package Question_5;
+
+public class MyInteger
+{
+    // ATTRIBUTES
+    private final int value;
+
+    // CONSTRUCTOR
+    public MyInteger(int value) { this.value = value; }
+
+    // GETTERS
+    public int getValue() { return value; }
+
+    // METHODS
+}
