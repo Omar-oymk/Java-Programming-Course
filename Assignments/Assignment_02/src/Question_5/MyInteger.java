d00d2a19a642b404d119a63292f94b074740cf65
omaroymk <72468295+oymk0@users.noreply.github.com>
2025-10-17 22:17:51 +0300

    Added getters constructors and nonstatic checkers

--- Assignments/Assignment_02/src/Question_5/MyInteger.java
+++ Assignments/Assignment_02/src/Question_5/MyInteger.java
@@ -12,4 +12,19 @@ public class MyInteger
     public int getValue() { return value; }
 
     // METHODS
+    //region NON-STATIC CHECKERS
+    public boolean isEven() { return value % 2 == 0; }
+    public boolean isOdd() { return value % 2 != 0; }
+    public boolean isPrime()
+    {
+        int count = 0;
+        for(int i = 1; i < value; i++)
+        {
+            if(value%i == 0) count++;
+        }
+        return count < 2;
+    }
+    //endregion
+    //region STATIC-CHECKERS
+    //endregion
 }
