81eca4991511667c769d51f69065208b6fa53f35
omaroymk <72468295+oymk0@users.noreply.github.com>
2025-10-17 23:50:59 +0300

    Added answers to question 8 and 9

---
+++ Assignments/Assignment_02/src/Question_8/Main.java
@@ -0,0 +1,12 @@
+package Question_8;
+
+/*
+8.
+If a method in a subclass has the same signature as a method in its superclass
+ with the same return type,
+ is the method overridden or overloaded?
+ */
+
+
+// ANSWER: it is overridden
+// overloaded is when a method has a different signature : same name + different parameter types
---
+++ Assignments/Assignment_02/src/Question_9/Main.java
@@ -0,0 +1,11 @@
+package Question_9;
+
+/*
+9.
+If a method in a subclass has the same signature as a method
+ in its superclass with a different return type,
+ will this be a problem?
+ */
+
+// ANSWER: yes it will cause compilation error because it will not even be overriding
+// since the return type is different to fix this u have to at least make it the same return type
