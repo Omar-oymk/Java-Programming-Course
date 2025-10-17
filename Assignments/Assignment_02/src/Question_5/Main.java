62e093ceaa4e419c42061d4feb1e68e15f97ce4e
omaroymk <72468295+oymk0@users.noreply.github.com>
2025-10-17 23:27:25 +0300

    Added Question_6

--- Assignments/Assignment_02/src/Question_5/Main.java
+++ Assignments/Assignment_02/src/Question_5/Main.java
@@ -1,5 +1,23 @@
 package Question_5;
 
+/*
+5. (The MyInteger class) Design a class named MyInteger. The class contains:
+ An int data field named value that stores the int value represented by this
+object.
+ A constructor that creates a MyInteger object for the specified int value. A
+getter method that returns the int value.
+ The methods isEven(), isOdd(), and isPrime() that return true if the value in
+this object is even, odd, or prime, respectively.
+ The static methods isEven(int), isOdd(int), and isPrime(int) that return true if
+the specified value is even, odd, or prime, respectively.
+ The static methods isEven(MyInteger), isOdd(MyInteger), and
+isPrime(MyInteger) that return true if the specified value is even, odd, or
+prime, respectively.
+ The methods equals(int) and equals(MyInteger) that return true if the value
+in this object is equal to the specified value.
+ Draw the UML diagram for the class and then implement the class. Write
+a client program that tests all methods in the class.
+ */
 
 public class Main
 {
