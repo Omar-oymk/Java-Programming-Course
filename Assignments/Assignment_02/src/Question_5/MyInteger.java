51a624d1c59605a7b1e2e6ad629293aa381ad7eb
omaroymk <72468295+oymk0@users.noreply.github.com>
2025-10-18 14:26:02 +0300

    learned a new approach for the isPrime method

--- Assignments/Assignment_02/src/Question_5/MyInteger.java
+++ Assignments/Assignment_02/src/Question_5/MyInteger.java
@@ -15,15 +15,29 @@ class MyInteger
     //region NON-STATIC CHECKERS
     public boolean isEven() { return value % 2 == 0; }
     public boolean isOdd() { return value % 2 != 0; }
+    // MATHS RULE:
+    // *If a number n has a divisor greater than √n, then it must also have one smaller than √n.*
+    // a better approach
     public boolean isPrime()
     {
-        int count = 0;
-        for(int i = 1; i <= value; i++)
-        {
-            if(value%i == 0) count++;
-        }
-        return count == 2;
+        // cause anything less than that is not a prime number
+        if(value <= 1) return false;
+        for(int i = 2; i < Math.sqrt(value); i++)
+            // if it is divisible directly that makes it a notPrime number
+            if(value % i == 0) return false;
+
+        return true;
     }
+//    public boolean isPrime()
+//    {
+//        int count = 0;
+//        for(int i = 1; i <= value; i++)
+//        {
+//            if(value%i == 0) count++;
+//        }
+//        return count == 2;
+//    }
+
 
     public boolean equals(int x) { return value == x; }
     public boolean equals(MyInteger x) { return value == x.getValue(); }
@@ -35,26 +49,40 @@ class MyInteger
     public static boolean isOdd(int value) { return value %2 != 0; }
     public static boolean isPrime(int value)
     {
-        int count = 0;
-        for(int i = 1; i <= value; i++)
-        {
-            if(value%i == 0) count++;
-        }
-        return count == 2;
+        if(value <= 1) return false;
+        for(int i = 2; i < Math.sqrt(value); i++)
+            if(value % i == 0) return false;
+        return true;
     }
+//    public static boolean isPrime(int value)
+//    {
+//        int count = 0;
+//        for(int i = 1; i <= value; i++)
+//        {
+//            if(value%i == 0) count++;
+//        }
+//        return count == 2;
+//    }
 
     // THESE TAKE AN OBJECT OF THE CLASS MyInteger (non-primitive datatype)
     public static boolean isEven(MyInteger value) { return value.getValue() % 2 == 0; }
     public static boolean isOdd(MyInteger value) { return value.getValue() % 2 != 0; }
     public static boolean isPrime(MyInteger value)
     {
-        int count = 0;
-        for(int i = 1; i <= value.getValue(); i++)
-        {
-            if(value.getValue()%i == 0) count++;
-        }
-        return count == 2;
+        if(value.getValue() <= 1) return false;
+        for(int i = 2; i < Math.sqrt(value.getValue()); i++)
+            if(value.getValue() % i == 0) return false;
+        return true;
     }
-    //endregion
+//    public static boolean isPrime(MyInteger value)
+//    {
+//        int count = 0;
+//        for(int i = 1; i <= value.getValue(); i++)
+//        {
+//            if(value.getValue()%i == 0) count++;
+//        }
+//        return count == 2;
+//    }
 
+    //endregion
 }
