d72512afbcee6a8d982487d8d02f8501a9b1584c
omaroymk <72468295+oymk0@users.noreply.github.com>
2025-11-21 21:36:25 +0200

    Added question

---
+++ Assignments/Assignment_04/src/Question_03/Main.java
@@ -0,0 +1,14 @@
+package Question_03;
+
+/*
+3. Write a test program that prompts the user to enter 5 numbers, stores them in an array list, and
+displays them in increasing order.
+*/
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
