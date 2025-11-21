10458b59dd9b50f06d705e570cb26c7ad7261d3b
omaroymk <72468295+oymk0@users.noreply.github.com>
2025-11-21 21:45:31 +0200

    Done with question 3

--- Assignments/Assignment_04/src/Question_03/Main.java
+++ Assignments/Assignment_04/src/Question_03/Main.java
@@ -5,10 +5,32 @@ package Question_03;
 displays them in increasing order.
 */
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Collections;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        // first initialize an empty arraylist of integers
+        ArrayList<Integer> arrayList = new ArrayList();
 
+        // prompt the user to enter the numbers
+        System.out.println("Please enter 5 numbers: ");
+        // then loop
+        for(int i = 0; i < 5; i ++)
+        {
+            arrayList.add((new Scanner(System.in)).nextInt());
+        }
+
+        // sort them
+        Collections.sort(arrayList);
+        // then display them
+        System.out.println("ArrayList ordered ascendingly: ");
+        for(Integer i : arrayList)
+        {
+            System.out.println(i);
+        }
     }
 }
