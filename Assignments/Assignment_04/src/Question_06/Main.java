79723d283375eee305ffabff22d64020c53b4936
omaroymk <72468295+oymk0@users.noreply.github.com>
2025-11-21 23:22:05 +0200

    done question 6

--- Assignments/Assignment_04/src/Question_06/Main.java
+++ Assignments/Assignment_04/src/Question_06/Main.java
@@ -6,10 +6,47 @@ randomly into the file. Integers are separated by spaces in the file. Read the d
 file and display the data in increasing order.
  */
 
+import java.io.IOException;
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Collections;
+
 public class Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
 
+        File file = new File("Text.txt");
+        if(!file.exists())
+        {
+            file.createNewFile();
+        }
+
+        PrintWriter output = new PrintWriter("Text.txt");
+
+        for(int i = 0; i < 100; i++)
+        {
+            output.print((new Random()).nextInt(100) + " ");
+        }
+
+        output.close();
+
+        Scanner input = new Scanner(file);
+        ArrayList<Integer> arrayList = new ArrayList();
+        while(input.hasNext())
+        {
+            arrayList.add(input.nextInt());
+        }
+        input.close();
+
+        // sort arraylist
+        Collections.sort(arrayList);
+        for(Integer i : arrayList)
+        {
+            System.out.print(i + " ");
+        }
     }
 }
