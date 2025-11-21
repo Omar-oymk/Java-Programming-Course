3b053b42bf36c45b208f8c7d2007b82599c3e42d
omaroymk <72468295+oymk0@users.noreply.github.com>
2025-11-21 23:08:55 +0200

    Added question 6

---
+++ Assignments/Assignment_04/src/Question_06/Main.java
@@ -0,0 +1,15 @@
+package Question_06;
+
+/*
+6. Write a program to create a file named Test.txt if it does not exist. Write 100 integers created
+randomly into the file. Integers are separated by spaces in the file. Read the data back from the
+file and display the data in increasing order.
+ */
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
