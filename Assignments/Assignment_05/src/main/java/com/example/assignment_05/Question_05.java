68a4a18700dd16795bf7ad083f0213621464e7da
omaroymk <72468295+oymk0@users.noreply.github.com>
2025-11-28 21:05:07 +0200

    Added question_05

---
+++ Assignments/Assignment_05/src/main/java/com/example/assignment_05/Question_05.java
@@ -0,0 +1,58 @@
+package com.example.assignment_05;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Question_05 extends Application
+{
+    public void start(Stage primaryStage) throws Exception
+    {
+        HBox pane = new HBox(10);
+        pane.setPadding(new Insets(5, 5, 5, 5));
+
+        Text text1 = new Text("Project -- 20%");
+        Text text2 = new Text("Quiz -- 10%");
+        Text text3 = new Text("Midterm -- 30%");
+        Text text4 = new Text("Final -- 40%");
+
+        Rectangle rect1 = new Rectangle(100, 200);
+        Rectangle rect2 = new Rectangle(100, 100);
+        Rectangle rect3 = new Rectangle(100, 300);
+        Rectangle rect4 = new Rectangle(100, 400);
+
+        VBox project = new VBox(10);
+        project.setAlignment(Pos.BOTTOM_CENTER);
+        project.getChildren().addAll(text1, rect1);
+
+        VBox quiz = new VBox(10);
+        quiz.setAlignment(Pos.BOTTOM_CENTER);
+        quiz.getChildren().addAll(text2, rect2);
+
+        VBox midterm = new VBox(10);
+        midterm.setAlignment(Pos.BOTTOM_CENTER);
+        midterm.getChildren().addAll(text3, rect3);
+
+        VBox finals = new VBox(10);
+        finals.setAlignment(Pos.BOTTOM_CENTER);
+        finals.getChildren().addAll(text4, rect4);
+
+        pane.getChildren().add(project);
+        pane.getChildren().add(quiz);
+        pane.getChildren().add(midterm);
+        pane.getChildren().add(finals);
+
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Question 05");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+}
