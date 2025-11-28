977775d768477ecaa071b4eb63b4a372851e2d1a
omaroymk <72468295+oymk0@users.noreply.github.com>
2025-11-28 20:27:12 +0200

    Added java image

---
+++ Assignments/Assignment_05/src/main/java/com/example/assignment_05/Question_03.java
@@ -0,0 +1,13 @@
+package com.example.assignment_05;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Question_03 extends Application
+{
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        
+    }
+}
