c303c045bc4d7e556dde75a4024b6582652d7be6
omaroymk <omaroymk@gmail.com>
2025-12-12 23:30:29 +0200

    Done question_03

--- Assignments/Assignment_06/src/main/java/com/example/assignment_06/Question_03.java
+++ Assignments/Assignment_06/src/main/java/com/example/assignment_06/Question_03.java
@@ -1,6 +1,8 @@
     package com.example.assignment_06;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Question_03 extends Application {
@@ -10,7 +12,18 @@ public class Question_03 extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) {
 
+        Pane pane = new Pane();
+
+        pane.setOnMouseMoved(event -> {
+            double x =  event.getX();
+            double y = event.getY();
+            stage.setTitle("(" + x + "," + y + ")");
+        });
+
+        Scene scene = new Scene(pane, 300, 200);
+        stage.setScene(scene);
+        stage.show();
     }
 }
